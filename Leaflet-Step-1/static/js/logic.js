275e01345fcc30c7b5291cdc033c6314739a59fa
jakemperry <Jakemperry@gmail.com>
2021-04-13 17:56:30 -0700

    updated logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -28,6 +28,12 @@ d3.json(earthquakesUrl).then(function(data) {
   function createFeatures(EQData){
     function onEachFeature(feature,layer){
       feature.properties.mag = +feature.properties.mag
+      // L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]],{
+      //   color: "red",
+      //   fillColor: "red",
+      //   fillOpacity: 0.75,
+      //   radius: (features.properties.mag * 100)
+      // });
       layer.bindPopup("<h3>"+ Number.parseFloat(feature.properties.mag).toPrecision(2) +"M, "+ feature.properties.place + "</h3><hr><p>"+ new Date(feature.properties.time)+"</p>");
     }
     var earthquakes = L.geoJSON(EQData, {
@@ -48,17 +54,17 @@ d3.json(earthquakesUrl).then(function(data) {
       accessToken: API_KEY
     });
   
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
-      id: "dark-v10",
+      id: "mapbox/dark-v11",
       accessToken: API_KEY
     });
   
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
       "Street Map": streetmap,
-      "Dark Map": darkmap
+      "Satellite Map": satellite
     };
   
     // Create overlay object to hold our overlay layer
